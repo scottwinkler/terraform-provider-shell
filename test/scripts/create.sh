7338b51527e7e0a00b758571880a29ccd453c8ae
Scott Winkler <Scott.Winkler@elliemae.com>
2018-10-11 13:23:41 -0700

    typo fix

--- test/scripts/create.sh
+++ test/scripts/create.sh
@@ -9,4 +9,4 @@ echo "stdin: ${IN}" #the old state, not useful for create step since the old sta
 /bin/cat <<END >ex.json
   {"commit_id": "b8f2b8b", "environment": "$yolo", "tags_at_commit": "sometags", "project": "someproject", "current_date": "09/10/2014", "version": "someversion"}
 END
-cat ex.json >&3
+cat ex.json
