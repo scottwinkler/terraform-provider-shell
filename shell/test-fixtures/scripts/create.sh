f3b116fd21ff80b577f46cf8ca27ee75c0c14e61
jasper.molenmaker <29455605+millmakerjm@users.noreply.github.com>
2019-09-18 09:38:53 +0200

    Enhanced test, test handling large JSON output

--- shell/test-fixtures/scripts/create.sh
+++ shell/test-fixtures/scripts/create.sh
@@ -5,9 +5,11 @@ echo "writing some error" >&2
 IN=$(cat)
 echo "stdin: ${IN}" #the old state, not useful for create step since the old state was empty
 
-#business logic
+#business logic, create a large string and write a JSON stucture to file
+TESTDATA=$(head -c $testdatasize < /dev/zero | tr '\0' '\141')
+
 /bin/cat <<END >${filename}
-  {"out1": "${out1}"}
+  {"data":"${TESTDATA}", "out1": "${out1}"}
 END
 
 cat ${filename} >&3
